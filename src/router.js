49e62b3262a759fe5a6e9b37e3481ed9d59a2824
瑾阳 <827126732@qq.com>
2019-12-19 19:13:58 +0800

    完成了报表功能的开发

--- src/router.js
+++ src/router.js
@@ -11,6 +11,7 @@ import Params from './components/goods/Params.vue'
 import List from './components/goods/List.vue'
 import Add from './components/goods/Add.vue'
 import Order from './components/order/Order.vue'
+import Report from './components/report/Report.vue'
 
 Vue.use(VueRouter)
 
@@ -40,6 +41,8 @@ const router = new VueRouter({
         path: '/goods/add', component: Add
       }, {
         path: '/orders', component: Order
+      }, {
+        path: '/reports', component: Report
       }]
     }
   ]
